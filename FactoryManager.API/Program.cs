667b613acfebfcf7e6aa66d82ae47d3b63d0055d
GideanMS <g1d34n@gmail.com>
2026-06-18 23:34:01 -0300

    endpoint testing with new machine list

--- FactoryManager.API/Program.cs
+++ FactoryManager.API/Program.cs
@@ -1,3 +1,5 @@
+using FactoryManager.Domain.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,15 +18,26 @@ app.UseHttpsRedirection();
 
 app.MapGet("/machines", () =>
 {
-    return new[]
+    var machines = new List<Machine>
     {
-        new
+        new()
         {
-            Id = 1,
+            Id = Guid.NewGuid(),
             Name = "Basic Furnace",
-            ProductionPerMinute = 10
+            ProductionPerMinute = 10,
+            IsActive = true
+        },
+
+        new()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Assembler MK-1",
+            ProductionPerMinute = 20,
+            IsActive = true
         }
     };
+
+    return machines;
 });
 
 app.MapGet("/", () =>
--- FactoryManager.Domain/Entities/Machine.cs
+++ FactoryManager.Domain/Entities/Machine.cs
@@ -1,9 +1,11 @@
+namespace FactoryManager.Domain.Entities;
+
 public class Machine
 {
-    public Guid Id { get; private set; }
-    public string Name { get; private set; }
-    public int ProductionPerMinute { get; private set; }
-    public bool IsActive { get; private set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int ProductionPerMinute { get; set; }
+    public bool IsActive { get; set; }
 
     public void Activate()
     {
